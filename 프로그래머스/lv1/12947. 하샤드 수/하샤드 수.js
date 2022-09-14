b80dbb2418cfc1063e3528637a0343c5768156c6
minngki <minzi970210@gmail.com>
2022-09-14 11:53:58 +0900

    add solution

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -2,3 +2,7 @@ function solution(x) {
     const xArr = (x+'').split('')
     return x%(xArr.reduce((acc,cur)=> acc+=parseInt(cur),0))===0
 }
+
+// 개선할 점
+// ! 1. 숫자형! 간단하게 쓰는 법 : +acc + +cur 이라고 써도 됐었음! 
+// ! 2. 구문 앞에 ! 붙는 것 : null, 0, nan, ''(빈배열), undefined 
