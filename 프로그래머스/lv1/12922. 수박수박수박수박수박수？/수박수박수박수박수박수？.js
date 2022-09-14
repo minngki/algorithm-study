7e30e3936abb92e2e00f2ba6fb96b37a168a5101
minngki <minzi970210@gmail.com>
2022-09-14 18:44:51 +0900

    add solution

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,3 +2,9 @@ function solution(n) {
     let str = '수박'
     return n%2===0? str.repeat(n/2) : str.repeat(n/2) + '수'
 }
+
+
+// 개선점: 또! 두 번 반복되는 것은 제외
+// function solution(n) {
+//     return '수박'.repeat(n/2) + (n%2===0? '수':'')
+// }
