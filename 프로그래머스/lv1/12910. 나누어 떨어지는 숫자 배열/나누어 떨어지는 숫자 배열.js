75807f138c8002665a6f10b29baae807cae7df69
minngki <minzi970210@gmail.com>
2022-09-14 18:23:08 +0900

    add solution

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -2,3 +2,9 @@ function solution(arr, divisor) {
     let divArr = arr.filter(num=> num%divisor===0).sort((a,b)=>a-b)
     return divArr.length === 0 ? [-1] : divArr
 }
+
+// 개선할 점 : sort는 엄청난 메모리를 소모하기 때문에 마지막에 반환할 때만 사용하는 것이 더 효율적이다.
+// function solution(arr, divisor) {
+//     let divArr = arr.filter(num=> num%divisor===0)
+//     return divArr.length === 0 ? [-1] : divArr.sort((a,b)=>a-b)
+// }
