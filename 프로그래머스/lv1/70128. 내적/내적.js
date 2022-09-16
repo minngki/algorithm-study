75d4f64363175e0813fdad8a1ce36bbd935f68fb
minngki <minzi970210@gmail.com>
2022-09-16 17:01:32 +0900

    add solution

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,3 +1,9 @@
 function solution(a, b) {
     return a.map((num,idx)=> num*b[idx]).reduce((acc,cur)=> acc+cur,0)
 }
+
+// 개선점: reduce도 idx가 있다!
+
+// function solution(a, b) {
+//     return a.reduce((acc,cur,idx)=> acc+= cur*b[idx],0)
+// }
