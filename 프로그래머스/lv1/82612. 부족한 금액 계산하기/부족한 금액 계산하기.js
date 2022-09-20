7953be86061ce7e24446cdc6bd9f1e2ed0b87263
MINJI KIM <94674673+minngki@users.noreply.github.com>
2022-09-20 15:50:18 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,9 +1,5 @@
 function solution(price, money, cnt) {
-    let sum=0
-    for (let i = 0 ; i<cnt+1 ; i++){
-        sum+=price*i
-    }
-    console.log(sum)
+    let sum = (price + price*cnt)*cnt / 2
     return sum > money ? sum-money : 0
     
     
