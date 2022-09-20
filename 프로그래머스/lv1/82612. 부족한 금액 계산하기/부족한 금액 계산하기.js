e4dfa682a89e0e9c5463538a2e2c784ca2df5546
minngki <minzi970210@gmail.com>
2022-09-20 15:58:04 +0900

    .

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,6 +1,16 @@
 function solution(price, money, cnt) {
-    let sum = (price + price*cnt)*cnt / 2
+    let sum=0
+    for (let i = 0 ; i<cnt+1 ; i++){
+        sum+=price*i
+    }
     return sum > money ? sum-money : 0
-    
-    
 }
+
+
+
+// 가우스 풀이 ! 등차수열합공식! 유용!! 
+// ! (a+l)n / 2
+// function solution(price, money, cnt) {
+//     let sum = (price + price*cnt)*cnt / 2
+//     return sum > money ? sum-money : 0 
+// }
