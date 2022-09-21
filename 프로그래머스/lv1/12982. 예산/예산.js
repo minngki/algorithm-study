3c541d6bcade24627885eab2f70d45f7282c4381
minngki <minzi970210@gmail.com>
2022-09-21 23:58:36 +0900

    add memo

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,24 +1,25 @@
 function solution(d, budget) {
-    d.sort((a, b) => a - b)
-    let num = 0
-    let answer = 0
-    let arr =[]
-    for (let i = 0 ; i < d.length ; i++){
-        num += d[i]
-        if (num<=budget) {
-            answer++
-            arr.push(d[i])
-            console.log(arr)
-        } else {
-            console.log('break?: ',answer)
-            break
-        }
+  d.sort((a, b) => a - b);
+  let num = 0;
+  let answer = 0;
+  let arr = [];
+  for (let i = 0; i < d.length; i++) {
+    num += d[i];
+    if (num <= budget) {
+      answer++;
+      arr.push(d[i]);
+      console.log(arr);
+    } else {
+      console.log('break?: ', answer);
+      break;
     }
-         return answer
+  }
+  return answer;
 }
 
-console.log(solution([1,8,9,10],27))
-
+console.log(solution([1, 8, 9, 10], 27));
+// ! 개선점 : 걍 문제 자체를 제대로 이해하지 못했음. 머리가 안 돌아가서 지금도 제대로 이해 못하는듯 진짜 멍청한데. 문제 자체를 이해 못해서 제대로 코딩을 못 하는듯
+// 코드를 몰라서가 아니라 걍 말귀를 못 알아듣는다니 너무 속상해
 
 // d.sort((a,b)=>a-b)
 // let lenIdx = 1
