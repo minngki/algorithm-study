a30e5f7ad25681d8b438cf1d4f7fc09a3f95c2be
MINJI KIM <94674673+minngki@users.noreply.github.com>
2022-09-27 14:05:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,5 +1,4 @@
 function solution(n, arr1, arr2) {
-    const newArr = Array(n).fill(0)
     const map1 = arr1.map(x=> x.toString(2).split('').map(y=> parseInt(y)))
     const map2 = arr2.map(x=> x.toString(2).split('').map(y=> parseInt(y)))
     let decrypt =[]
@@ -12,6 +11,12 @@ function solution(n, arr1, arr2) {
         }
         
         decrypt.push(map1[i].map((x,idx)=> x+map2[i][idx] === 0 ? ' ' : '#').join(''))
+
     }
     return decrypt
 }
+
+// 개선점: 논리연산자 사용과 배열 변환을 하지 않고 string으로 충분히 할 수 있었다.
+// function solution2(n,arr1,arr2) {
+//     return arr1.map((x,idx)=> (x|arr2[i]).toString(2).padStart(n,'0').replace(/1/g,'#').replace(/0/g,' '))
+// }
