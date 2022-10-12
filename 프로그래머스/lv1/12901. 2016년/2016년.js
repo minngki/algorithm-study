af390fefdcf0089845694c2ee6cd8fa82ca9d3ba
MINJI KIM <94674673+minngki@users.noreply.github.com>
2022-10-12 23:31:18 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,19 +1,14 @@
 function solution(a, b) {
     const day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
     const date = [31,29,31,30,31,30,31,31,30,31,30,31]
-    let dayIdx = 0
-    if (a>1) {
-        let firstDay = date.reduce((acc,cur,idx)=> idx<a-1 ? acc += cur : acc+= 0 )
-    dayIdx = (firstDay+b)%7 -1
-    }
-    else {
-        dayIdx = b%7-1
-    }
-    return dayIdx === -1 ? day[6] : day[dayIdx]
-    
+    let firstDay = date.reduce((acc,cur,idx)=> idx<a ? acc += cur : acc+= 0 )
+
+    let ans = (b+firstDay-1)%day.length
+    console.log("firstDay: ",firstDay, " ans: ", ans)
+    return day[ans+4]
+
 }
 
-solution(2,29)
 
 //     while (firstDay>6) {
 //         firstDay = parseInt(firstDay/day.length)
@@ -26,4 +21,4 @@ solution(2,29)
 //     // console.log(day[temp])
 //     // return day[temp]
 
-// 따로 하려니까 어려웠던 듯 삽질쟁이?
+// 따로 하려니까 어려웠던 듯
