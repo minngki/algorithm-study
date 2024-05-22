282059fa2e2c64bbf46ab7d44dd57fc3f705912a
MINJI KIM <94674673+minngki@users.noreply.github.com>
2024-05-22 11:27:32 +0900

    Update 추억 점수.js

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -6,3 +6,13 @@ function solution(name, yearning, photo) {
     }
     return answer
 }
+
+// 새 풀이
+function solution(name, yearning, photo) {
+    const answer = []
+    for (let row of photo){
+        let total = row.reduce((acc,cur)=> acc + (yearning[name.indexOf(cur)] ?? 0), 0)
+        answer.push(total)
+    }
+    return answer
+}
