2eff5b3d4728f36e5f62c4f8956067d88958faae
MINJI KIM <94674673+minngki@users.noreply.github.com>
2024-07-23 11:16:40 +0900

    Update 수 정렬하기.py (bubble sort)

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -7,5 +7,25 @@ for i in range(num):
 
 for num in sorted(num_list):
     print(num)
+
+
+
+def bubble_sort():
+    # 버블정렬 문제풀이입니다.
+    N=int(input())
+    A=[0]*N
     
+    for i in range(N):
+        A[i]=int(input())
+    
+        
+    for i in range(N-1):
+        for j in range(N-1-i):
+            if A[j] > A[j+1]:
+                temp = A[j]
+                A[j] = A[j+1]
+                A[j+1] = temp
+                
+    for i in range(N):
+        print(A[i])
    
